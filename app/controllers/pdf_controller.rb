ac40f8603251c1f4ca3421a797c4d9b04d435825
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-02-16 13:20:03 -0300

    Added send file in controller
    - removed unused code
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -3,12 +3,17 @@ class PdfController < ApplicationController
   end
 
   def generate
-    generatePDF
+    generatePDF('http://localhost:3000') do |path|
+      send_file(path, filename: 'output.pdf', type: 'application/pdf', disposition: 'attachment')
+    end
   end
 
   private
 
-  def generatePDF
-    system("yarn createPDF http://localhost:3000 output.pdf")
+  def generatePDF(url)
+    tmp = Tempfile.new("tmp/pdf-chrome-puppeteer-#{Time.now.to_i}")
+    system("yarn createPDF #{Shellwords.escape(url)} #{Shellwords.escape(tmp.path)}")
+
+    yield(tmp.path) if block_given?
   end
 end
